94d8d2e7dc538cbedd61f57a0301c3fdb1d8ca92
oRIMURUTEMPESTo <gitcarlos2024@gmail.com>
2024-09-30 20:04:58 -0400

    correccion ventas

--- database/migrations/2024_09_30_223808_create_ventas_table.php
+++ database/migrations/2024_09_30_223808_create_ventas_table.php
@@ -4,24 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateVentasTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('ventas', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id();  // Identificador de la venta
+            $table->foreignId('id_cliente')->constrained('clientes', 'id_cliente')->onDelete('cascade');  // Relación con clientes
+            $table->decimal('monto_total', 10, 2);  // Monto total de la venta
+            $table->timestamp('fecha_venta')->useCurrent();  // Fecha de la venta
+            $table->text('detalles')->nullable();  // Detalles adicionales si es necesario
+            $table->timestamps();  // created_at y updated_at automáticos
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('ventas');
     }
-};
+}
